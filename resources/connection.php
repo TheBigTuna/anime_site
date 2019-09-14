3f12d96996ede6f663ed8ace44b2eb5294165c70
octavius moore <mooreoctavius94@gmail.com>
2019-09-14 19:54:46 -0400

    Set up work environment
    Set up work environment

---
+++ resources/connection.php
@@ -0,0 +1,8 @@
+<?php
+// connection file to local DB
+$servername = "localhost";
+$username = "root";
+$password = "deandre94";
+$conn = mysqli_connect($servername, $username, $password);
+echo "Connected";
+?>
