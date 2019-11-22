3ceb4f7ee1999c250a9c090918db02a250353b68
octavius <mooreoctavius94@gmail.com>
2019-11-21 21:00:02 -0500

    update 11/21/2019
    update 11/21/2019

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
     <html lang="en">
         <head>
-            <title>Anime Site :D</title>
+            <title>ANIMEROOMS</title>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="icon" href="images/halfLogo.png">
             <link href="style/bootstrap.css" rel="stylesheet">
             <link href="style/main.css" rel="stylesheet">
             <script src="script/jquery-3.4.1.js"></script>
@@ -11,6 +12,14 @@
 			<script src="script/main.js"></script>
             <!-- <?php include("resources/connection.php");?> -->
             <!-- <?php include("api_config.php"); ?> -->
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-152462977-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-152462977-1');
+        </script>
         </head>
     <body>
     <?php
