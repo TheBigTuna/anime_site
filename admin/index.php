12dbae9e679b2321f380ae3e44bbc91dea148690
octavius <mooreoctavius94@gmail.com>
2019-11-24 15:37:11 -0500

    update 11/24/2019 #2
    update 11/24/2019 #2

---
+++ admin/index.php
@@ -0,0 +1,9 @@
+<?php
+ include('../navbar.php');
+?>
+
+<div class="container">
+    <a href="createPost.php">
+        <button type="button" class="btn btn-primary btn-block adminBtn">Create Post</button>
+    </a>
+</div>
