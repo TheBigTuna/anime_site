6beaa7e2b987ed88dc856ba81f9815107a0e0b88
octavius <mooreoctavius94@gmail.com>
2019-11-25 17:31:36 -0500

    update 11/25/2019
    update 11/25/2019

--- admin/createPost.php
+++ admin/createPost.php
@@ -3,5 +3,13 @@
 ?>
 
 <div class="container">
-    <textarea rows="500" cols="50"></textarea>
+        <h4>Enter Text</h4>
+        <input type="text" id="createPostEnterTitle" class="form-control" placeholder="Enter Title">
+        <h4>Upload Content</h4>
+        <textarea id="createPostTextBox" rows="20" cols="130"></textarea>
+        <h4>Select Tags</h4>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+            <label class="form-check-label" for="defaultCheck1">Default checkbox</label>
+        </div>
 </div>
