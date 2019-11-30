c08458f8bfab1dad7c456fa0c9cd42d007f4a29c
octavius <mooreoctavius94@gmail.com>
2019-11-29 19:30:32 -0500

    update 11/29 2019 #3
    update 11/29 2019 #3

--- admin/managePost.php
+++ admin/managePost.php
@@ -6,6 +6,7 @@
 ?>
 
 <div class="container">
+    <!-- Manage Post Table -->
     <table class="table" id="managePostTable">
         <thead>
             <tr>
@@ -18,10 +19,10 @@
         </thead>
         <tbody>
         <?php
+            // Query to fetch currently available articles
             $FetchArticles = "SELECT * FROM omoore94_animerooms.cmsarticles AS A INNER JOIN omoore94_animerooms.cmsarticlesinfo AS AI ON AI.ID = A.ID ORDER BY A.ID DESC";        
             $FetchArticlesResult = mysqli_query($conn, $FetchArticles);
             while($row = mysqli_fetch_assoc($FetchArticlesResult)){
-                print_r($row);
             ?>
                 <tr class="managePostTR" onclick="openModal('<?= $row['ID']; ?>', '<?= $row['ArticleType']; ?>', '<?= $row['Text']; ?>', '<?= $row['Tag1']; ?>', '<?= $row['Tag2']; ?>', '<?= $row['Tag3']; ?>','<?= $row['Tag4']; ?>','<?= $row['Tag5']; ?>','<?= $row['Img1']; ?>','<?= $row['Img2']; ?>','<?= $row['Img3']; ?>','<?= $row['Img4']; ?>','<?= $row['Img5']; ?>')">
                 <th><?= $row['ID']; ?></th>
@@ -39,15 +40,13 @@
 
 <!-- Manage Post Modal -->
 <div class="modal fade" id="manageModalPost" tabindex="-1" role="dialog">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog" style="max-width: 80%; margin-top 4rem;" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="manageModalPostLabel">Post Information</h5>
+      <div class="modal-body">
+      <h5 class="modal-title" id="manageModalPostLabel">Post Information</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-      </div>
-      <div class="modal-body">
       <table class="table">
         <thead>
             <tr>
@@ -72,11 +71,8 @@
             </tr>
         </tbody>
         </table>
-            
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Update</button>
+        <button type="button" class="btn btn-primary">Delete</button>
       </div>
     </div>
   </div>
@@ -90,11 +86,13 @@
 
         var modalInfoArray = [Id, Type, Text, Tag1, Tag2, Tag3, Tag4, Tag5, Img1, Img2, Img3, Img4, Img5];
         var modalPostOutput = "";
-
+        
+        // Concate table data to modalpostoutput variable
         for(var i=0; i < modalInfoArray.length; i++){
             modalPostOutput += "<td>" + modalInfoArray[i] + "</td>";
         }
-        console.log(modalPostOutput);
+
+        // Output Post Data to table
         $("#ModalPostOutput").html(modalPostOutput);
     }
 </script> 
