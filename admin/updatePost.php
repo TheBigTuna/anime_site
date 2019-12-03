c6f96216c16af241a2f6f9304f4fae87e5262791
octavius <mooreoctavius94@gmail.com>
2019-12-02 22:36:55 -0500

    update 12/2/2019
    update 12/2/2019

--- admin/updatePost.php
+++ admin/updatePost.php
@@ -6,12 +6,17 @@ include('../navbar.php');
 ?>
 
 <div class="container">
+    <?php
+        // Query to fetch currently available articles
+        $FetchArticles = "SELECT * FROM omoore94_animerooms.cmsarticles AS A INNER JOIN omoore94_animerooms.cmsarticlesinfo AS AI ON AI.ID = A.ID WHERE A.ID = 2 ORDER BY A.ID DESC";        
+        $FetchArticlesResult = mysqli_query($conn, $FetchArticles);
+    ?>
+    <h3>Current Table</h3>
     <table class="table" id="UpdatePostCurrentValues">
         <thead>
             <tr>
                 <th scope="col">ID</th>
                 <th scope="col">Type</th>
-                <th scope="col">Text</th>
                 <th scope="col">Tag1</th>
                 <th scope="col">Tag2</th>
                 <th scope="col">Tag3</th>
@@ -25,25 +30,48 @@ include('../navbar.php');
             </tr>
         </thead>
         <tbody>
+        <?php
+            while($row = mysqli_fetch_assoc($FetchArticlesResult)){
+                $PostText = $row['Text'];
+            ?>
             <tr>
-            <tr id="ModalPostOutput"></tr>
+                <td><?= $row['ID']; ?></td>
+                <td><?= $row['ArticleType']; ?></td>
+                <td><?= $row['Tag1']; ?></td>
+                <td><?= $row['Tag2']; ?></td>
+                <td><?= $row['Tag3']; ?></td>
+                <td><?= $row['Tag4']; ?></td>
+                <td><?= $row['Tag5']; ?></td>
+                <td><?= $row['Img1']; ?></td>
+                <td><?= $row['Img2']; ?></td>
+                <td><?= $row['Img3']; ?></td>
+                <td><?= $row['Img4']; ?></td>
+                <td><?= $row['Img5']; ?></td>
+
             </tr>
+            <?php
+            }
+        ?>
         </tbody>
     </table>
+
+    <!-- Output the current Text -->
+    <h3>Current Text</h3>
+    <p id="ViewCurrentText"><?= $PostText; ?></p>
 
 
     <form action="insertPost.php" method="POST">
     <!-- Enter Title -->
-        <h4>Enter Title</h4>
+        <h3>Enter Title</h3>
         <input type="text" id="UpdatePostEnterTitle" name="Title" class="form-control" placeholder="Enter Title">
 
         <!-- Enter Subtitle -->
-        <h4>Enter Subtitle</h4>
+        <h3>Enter Subtitle</h3>
         <input type="text" id="UpdatePostEnterSubtitle" name="SubTitle" class="form-control" placeholder="Enter Subtitle">
 
 
     <!-- Select The Type of Post -->
-        <h4>Post Type</h4>
+        <h3>Post Type</h3>
             <select class="form-control" name="PostType" id="UpdatePostSelectPostType">
                 <option selected disabled></option>
                 <option value="Article1">Article Type 1</option>
@@ -56,11 +84,11 @@ include('../navbar.php');
 
     
     <!-- Article Content -->
-        <h4>Upload Text/Content</h4>
+        <h3>Upload Text/Content</h3>
         <textarea id="UpdatePostTextBox" name="PostText" rows="15" cols="130"></textarea>
 
     <!-- Tag article -->
-        <h4>Select Tags</h4>
+        <h3>Select Tags</h3>
         <small>Select up to 5 tags</small>
         <?php
         $FetchTags = "SELECT * FROM omoore94_animerooms.cmstags";        
@@ -80,7 +108,7 @@ include('../navbar.php');
         </div>
 
     <!-- Add Images to article -->
-        <h4>Upload Images</h4>
+        <h3>Upload Images</h3>
         <input type="file" name="Img[]" enctype="multipart/form-data">
         <input type="file" name="Img[]" enctype="multipart/form-data">
         <input type="file" name="Img[]" enctype="multipart/form-data">
--- admin/verifyLogin.php
+++ admin/verifyLogin.php
@@ -2,14 +2,17 @@
     session_start();
     include('../resources/connection.php');
 
+    // Search DB for admin credentials
     $VerifyAdminUser = "SELECT * FROM omoore94_animerooms.admincredentials";        
     $VerifyAdminUserResult = mysqli_query($conn, $VerifyAdminUser);
     while($row = mysqli_fetch_assoc($VerifyAdminUserResult)){
+        // Checks for a  username and password
         if($_POST['adminUser'] == $row['User']){
             $Passwords = $row['Password'];
         }
     }
 
+    // Redirects the user depending if the passwords match or not
     if($_POST['adminLogin'] === $Passwords){
         $_SESSION['IsAdmin'] = true;
         header('Location: /anime_site/admin/main.php');
