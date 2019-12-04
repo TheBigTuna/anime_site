22459e714a883a75a87b524de35033d6b253735b
octavius <mooreoctavius94@gmail.com>
2019-12-03 20:53:09 -0500

    update 12/3/2019 #2
    update 12/3/2019 #2

--- index.php
+++ index.php
@@ -6,7 +6,14 @@
     <div class="PageContainer">
         <h6 class="PageContainerHeadText">Recent Posts</h6>
         <div class="ArticlePreviewBG">
-            TEST
+            <div class="row">
+                <div class="col-sm-12 col-md-2" style="background-color: white;">
+                    <img src="images/comics.jpg" class="ArticlePreviewImg" style="max-width: 120px; max-height: 120px;"> 
+                </div>
+                <div class="col-sm-12 col-md-10" style="background-color: orange;">
+                    <h6 class="ArticlePreviewTitle">THIS IS A TESTTTTT Title</h6>
+                </div>
+            </div>
         </div>
     </div>
 </div>
