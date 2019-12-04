ef32321f75dce28b6176812c0667099a0900c8f2
octavius <mooreoctavius94@gmail.com>
2019-12-03 21:45:55 -0500

    update 12/3/2019 #3
    update 12/3/2019 #3

--- index.php
+++ index.php
@@ -7,11 +7,14 @@
         <h6 class="PageContainerHeadText">Recent Posts</h6>
         <div class="ArticlePreviewBG">
             <div class="row">
-                <div class="col-sm-12 col-md-2" style="background-color: white;">
-                    <img src="images/comics.jpg" class="ArticlePreviewImg" style="max-width: 120px; max-height: 120px;"> 
+                <div class="col-sm-12 col-lg-2" style="background-color: white;">
+                    <div class="ArticlePreviewImgContainer">
+                        <img src="images/comics.jpg" class="ArticlePreviewImg"> 
+                    </div>
                 </div>
-                <div class="col-sm-12 col-md-10" style="background-color: orange;">
-                    <h6 class="ArticlePreviewTitle">THIS IS A TESTTTTT Title</h6>
+                <div class="col-sm-12 col-lg-10" style="background-color: orange;">
+                    <h6 class="ArticlePreviewTitle">Novel 'Josee to Tora to Sakana-tachi' Gets Anime Film in 2020</h6>
+                    <p class="ArticlePreviewSubTitle">Novel 'Josee to Tora to Sakana-tachi' Gets Anime Film in 2020</p>
                 </div>
             </div>
         </div>
