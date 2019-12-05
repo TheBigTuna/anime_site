a9ea73d57378e36b0db45d05eb8d024ee48f69a4
octavius <mooreoctavius94@gmail.com>
2019-12-04 20:22:08 -0500

    update 12/4/2019
    update 12/4/2019

--- index.php
+++ index.php
@@ -6,17 +6,33 @@
     <div class="PageContainer">
         <h6 class="PageContainerHeadText">Recent Posts</h6>
         <div class="ArticlePreviewBG">
+        <?php
+            // Query to fetch currently available articles
+            $FetchArticles = "SELECT * FROM omoore94_animerooms.cmsarticles AS A INNER JOIN omoore94_animerooms.cmsarticlesinfo AS AI ON AI.ID = A.ID ORDER BY A.ID DESC";        
+            $FetchArticlesResult = mysqli_query($conn, $FetchArticles);
+            while($row = mysqli_fetch_assoc($FetchArticlesResult)){
+        ?>
             <div class="row">
                 <div class="col-sm-12 col-lg-2" style="background-color: white;">
                     <div class="ArticlePreviewImgContainer">
-                        <img src="images/comics.jpg" class="ArticlePreviewImg"> 
+                        <img src="images/<?= $row['Img1']; ?>" class="ArticlePreviewImg"> 
                     </div>
                 </div>
                 <div class="col-sm-12 col-lg-10" style="background-color: orange;">
-                    <h6 class="ArticlePreviewTitle">Novel 'Josee to Tora to Sakana-tachi' Gets Anime Film in 2020</h6>
-                    <p class="ArticlePreviewSubTitle">Novel 'Josee to Tora to Sakana-tachi' Gets Anime Film in 2020</p>
+                    <h6 class="ArticlePreviewTitle"><?= $row['ArticleName']; ?></h6>
+                    <p class="ArticlePreviewSubTitle"><?= $row['ArticleName']; ?></p>
+                    <?php
+                        if(strlen($row['Text']) > 400){
+                            $row['Text'] = substr_replace($row['Text'], "<span style='font-size:18px; font-weight: 600;'>.....</span>", 400);
+                        }
+                    ?>
+                    <small><?= $row['Text']; ?></small>
+                    <p>View More</p>
                 </div>
             </div>
+        <?php
+            }
+        ?>
         </div>
     </div>
 </div>
