c6a32d44d74ccda2149296811a141b4b2c58a847
octavius <mooreoctavius94@gmail.com>
2019-12-05 17:29:00 -0500

    update 12/5/2019
    update 12/5/2019

--- index.php
+++ index.php
@@ -12,24 +12,25 @@
             $FetchArticlesResult = mysqli_query($conn, $FetchArticles);
             while($row = mysqli_fetch_assoc($FetchArticlesResult)){
         ?>
+        <div class="ArticlePreviewCard">
             <div class="row">
-                <div class="col-sm-12 col-lg-2" style="background-color: white;">
+                <div class="col-sm-12 col-lg-2">
                     <div class="ArticlePreviewImgContainer">
-                        <img src="images/<?= $row['Img1']; ?>" class="ArticlePreviewImg"> 
+                        <img src="images/<?= $row['Img1']; ?>" style="max-width:100%; max-height: 100%;" class="ArticlePreviewImg"> 
                     </div>
                 </div>
-                <div class="col-sm-12 col-lg-10" style="background-color: orange;">
+                <div class="col-sm-12 col-lg-10">
                     <h6 class="ArticlePreviewTitle"><?= $row['ArticleName']; ?></h6>
                     <p class="ArticlePreviewSubTitle"><?= $row['ArticleName']; ?></p>
                     <?php
                         if(strlen($row['Text']) > 400){
-                            $row['Text'] = substr_replace($row['Text'], "<span style='font-size:18px; font-weight: 600;'>.....</span>", 400);
+                            $row['Text'] = substr_replace($row['Text'], "<span style='font-size:18px; font-weight: 600;'>....</span><a href='#'><span style='font-size:13px; color: #1bb1dc;'> Read More </span></a>", 400);
                         }
                     ?>
                     <small><?= $row['Text']; ?></small>
-                    <p>View More</p>
                 </div>
             </div>
+        </div>
         <?php
             }
         ?>
