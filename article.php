deba06e0c3bbf87c489af748271d73d688689dc2
octavius <mooreoctavius94@gmail.com>
2019-12-08 12:45:22 -0500

    update 12/8/2019
    update 12/8/2019

--- article.php
+++ article.php
@@ -1,5 +1,23 @@
 <?php
  include('navbar.php');
 ?>
-
-Article
+<div class="row">
+    <div class="container">
+        <?php
+            // Query to fetch currently available articles
+            $FetchArticles = "SELECT * FROM omoore94_animerooms.cmsarticles AS A INNER JOIN omoore94_animerooms.cmsarticlesinfo AS AI ON AI.ID = A.ID WHERE A.ID = 4 ORDER BY A.ID DESC";        
+            $FetchArticlesResult = mysqli_query($conn, $FetchArticles);
+            while($row = mysqli_fetch_assoc($FetchArticlesResult)){
+            ?>
+                <div class="ArticlePageBG">
+                    <div class="ArticlePageTitleBG">
+                        <h2 class="ArticlePageTitle"><?= $row['ArticleName']; ?></h2>
+                    </div>
+                    <h6 class="ArticlePageAuthor">By <span style="color: #0B5AA3;"><?= $row['User']; ?></span></h6>
+                    <?php print_r($row); ?>
+                </div>
+            <?php
+            }
+        ?>
+    </div> 
+</div> 
--- index.php
+++ index.php
@@ -2,38 +2,40 @@
  include('navbar.php');
 ?>
 
-<div class="container">
-    <div class="ArticlePreviewBG">
-    <?php
-        // Query to fetch currently available articles
-        $FetchArticles = "SELECT * FROM omoore94_animerooms.cmsarticles AS A INNER JOIN omoore94_animerooms.cmsarticlesinfo AS AI ON AI.ID = A.ID ORDER BY A.ID DESC";        
-        $FetchArticlesResult = mysqli_query($conn, $FetchArticles);
-        while($row = mysqli_fetch_assoc($FetchArticlesResult)){
-    ?>
-    <div class="ArticlePreviewCard">
-        <div class="row">
-            <div class="col-sm-12 col-lg-2">
-                <div class="ArticlePreviewImgContainer">
-                    <a href="/anime_site/article.php?ID=<?= $row['ID']; ?>"><img src="images/<?= $row['Img1']; ?>" class="ArticlePreviewImg"></a>
+<div class="row">
+    <div class="container">
+        <div class="ArticlePreviewBG">
+        <?php
+            // Query to fetch currently available articles
+            $FetchArticles = "SELECT * FROM omoore94_animerooms.cmsarticles AS A INNER JOIN omoore94_animerooms.cmsarticlesinfo AS AI ON AI.ID = A.ID ORDER BY A.ID DESC";        
+            $FetchArticlesResult = mysqli_query($conn, $FetchArticles);
+            while($row = mysqli_fetch_assoc($FetchArticlesResult)){
+        ?>
+        <div class="ArticlePreviewCard">
+            <div class="row">
+                <div class="col-sm-12 col-lg-2">
+                    <div class="ArticlePreviewImgContainer">
+                        <a href="/anime_site/article.php?ID=<?= $row['ID']; ?>"><img src="images/<?= $row['Img1']; ?>" class="ArticlePreviewImg"></a>
+                    </div>
                 </div>
-            </div>
-            <div class="col-sm-12 col-lg-10">
-                <div class="ArticlePreviewContent">
-                    <a href="/anime_site/article.php?ID=<?= $row['ID']; ?>"><h5 class="ArticlePreviewTitle"><?= $row['ArticleName']; ?></h5></a>
-                    <!-- <h6 class="ArticlePreviewSubTitle"><?= $row['ArticleSubTitle']; ?></h6> -->
-                    <?php
-                        if(strlen($row['Text']) > 250){
-                            $row['Text'] = substr_replace($row['Text'], "<span style='font-size:16px; font-weight: 600;'>....</span><a href='/anime_site/article.php?ID=" . $row['ID'] . "'><span style='font-size:13px; color: #1bb1dc;'> Read More </span></a>", 250);
-                        }
-                    ?>
-                    <p class="ArticlePreviewText"><?= $row['Text']; ?></p>
-                    <p class="ArticlePreviewInfo"><?= date("F d, Y g:i A",strtotime($row['Timestamp'])); ?> by <span style="font-weight: 600;"><?= $row['User']; ?></span></p>
+                <div class="col-sm-12 col-lg-10">
+                    <div class="ArticlePreviewContent">
+                        <a href="/anime_site/article.php?ID=<?= $row['ID']; ?>"><h5 class="ArticlePreviewTitle"><?= $row['ArticleName']; ?></h5></a>
+                        <!-- <h6 class="ArticlePreviewSubTitle"><?= $row['ArticleSubTitle']; ?></h6> -->
+                        <?php
+                            if(strlen($row['Text']) > 250){
+                                $row['Text'] = substr_replace($row['Text'], "<span style='font-size:16px; font-weight: 600;'>....</span><a href='/anime_site/article.php?ID=" . $row['ID'] . "'><span style='font-size:13px; color: #1bb1dc;'> Read More </span></a>", 250);
+                            }
+                        ?>
+                        <p class="ArticlePreviewText"><?= $row['Text']; ?></p>
+                        <p class="ArticlePreviewInfo"><?= date("F d, Y g:i A",strtotime($row['Timestamp'])); ?> by <span style="font-weight: 600;"><?= $row['User']; ?></span></p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <?php
-        }
-    ?>
+        <?php
+            }
+        ?>
+        </div>
     </div>
 </div>
