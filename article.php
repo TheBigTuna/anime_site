42298e415246ec22b2f43a652b87a742debd72f4
octavius <mooreoctavius94@gmail.com>
2019-12-09 18:02:16 -0500

    update 12/9/2019 #2
    update 12/9/2019 #2

--- article.php
+++ article.php
@@ -10,11 +10,14 @@
             while($row = mysqli_fetch_assoc($FetchArticlesResult)){
             ?>
                 <div class="ArticlePageBG">
+                    <!-- Article Title -->
                     <div class="ArticlePageTitleBG">
                         <h2 class="ArticlePageTitle"><?= $row['ArticleName']; ?></h2>
                     </div>
+                    <!-- Article Author -->
                     <h6 class="ArticlePageAuthor">By <span style="color: #0B5AA3;"><?= $row['User']; ?></span></h6>
-                    <h6><?= date("F d, Y g:i A",strtotime($row['Timestamp'])); ?></h6>
+                    <!-- Timestamp from when the article was created -->
+                    <h6 class="ArticlePageTimestamp"><?= date("F d, Y g:i A",strtotime($row['Timestamp'])); ?></h6>
                     <div id="ArticleHeaderSection">
                         <div class="row">
                             <div class="col-sm-12 col-lg-8">
@@ -23,8 +26,12 @@
                                 </div>
                             </div>
                             <div class="col-sm-12 col-lg-4">
-                                <div class="ArticleSuggestedArticlesBG">
-                                    Test
+                                <div class="ArticleSuggestedArticlesBG" style="background-color: lightblue; width: 100%; height: 100%;">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            TEST
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
