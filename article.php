773091663d8a8e44514e8898159a06e4279de85e
octavius <mooreoctavius94@gmail.com>
2019-12-12 21:16:29 -0500

    update 12/12/2012
    update 12/12/2012

--- article.php
+++ article.php
@@ -5,7 +5,7 @@
     <div class="container">
         <?php
             // Query to fetch currently available articles
-            $FetchArticles = "SELECT * FROM omoore94_animerooms.cmsarticles AS A INNER JOIN omoore94_animerooms.cmsarticlesinfo AS AI ON AI.ID = A.ID WHERE A.ID = 4 ORDER BY A.ID DESC";        
+            $FetchArticles = "SELECT * FROM omoore94_animerooms.cmsarticles AS A INNER JOIN omoore94_animerooms.cmsarticlesinfo AS AI ON AI.ID = A.ID WHERE A.ID = 2 ORDER BY A.ID DESC";        
             $FetchArticlesResult = mysqli_query($conn, $FetchArticles);
             $ArticleRow = array();
             while($row = mysqli_fetch_assoc($FetchArticlesResult)){
@@ -25,7 +25,7 @@
                         <div class="row">
                             <div class="col-sm-12 col-lg-8">
                                 <div class="ArticleMainPictureBG">
-                                    <a href="/anime_site/article.php?ID=<?= $ArticleRow[0]['ID']; ?>"><img src="images/<?= $ArticleRow[0]['Img1']; ?>" style="height: 100%; width: 100%; float: right;"></a>
+                                    <a href="/anime_site/article.php?ID=<?= $ArticleRow[0]['ID']; ?>"><img src="images/<?= $ArticleRow[0]['Img1']; ?>" style="height: 450px; width: 100%; object-fit: cover; float: right;"></a>
                                 </div>
                             </div>
                             <div class="col-sm-12 col-lg-4">
@@ -55,6 +55,18 @@
                                         ?>
                                         </div>
                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12 col-lg-8">
+                                <div class="ArticleBodyContainer" style="background-color: lightblue;">
+                                    <h5>Test 1234</h5>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-lg-4">
+                                <div class="ArticleSidePanel">
+                                    <h5>TEST</h5>
                                 </div>
                             </div>
                         </div>
