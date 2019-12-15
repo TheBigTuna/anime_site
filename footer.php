11340dceb9cf784da2ab70b0a778eca901118d6d
octavius <mooreoctavius94@gmail.com>
2019-12-15 11:36:17 -0500

    update 12/15/2019
    update 12/15/2019

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <div class="row">
         <div class="container">
             <div class="col-sm-12">
-                <div class="footerBackground">
+                <!-- <div class="footerBackground">
                     <h6>Sign Up to the animerooms.com newsletter</h6>
                     <div class="input-group">
                         <input type="text" class="form-control FooterNewsletterNavbar" placeholder="Enter Email Address">
@@ -9,7 +9,7 @@
                             <button class="input-group-text">Submit</button>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
--- layout/contact.php
+++ layout/contact.php
@@ -1,2 +1,14 @@
 <?php include('../navbar.php'); ?>
-<h1>Contact</h1>
+
+<div class="container">
+    <h2>Contact</h2>
+    
+<p>If you would like to contact me for business inquires, website/artcle suggestions or feedback, feel free to contact me at:</p>
+<h5>animeroomsus@gmail.com</h5>
+
+I apologize if I do not reply to emails or social media messages right away. I work 40 hours a week and have some freelance side gigs. I will reply as soon as I can. If you do not hear from me after some time, feel free to message me again.
+
+**As of right now, I am not accepting guest posts from outside writers for Rai’s Anime Blog. However, I may be willing to write my own guest posts for other anime websites.**
+
+If you want to reach out to me on social media to chat or say hi, or just want to follow my accounts for more anime, click the icons below. I am the most active on Twitter!
+</div>
