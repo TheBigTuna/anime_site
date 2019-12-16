e4c6c48f869af6ae114897c95083de04ad01fc0f
octavius <mooreoctavius94@gmail.com>
2019-12-16 18:01:23 -0500

    update 12/16/2019 #2
    update 12/16/2019 #2

--- layout/about.php
+++ layout/about.php
@@ -3,9 +3,21 @@
     <div class="AboutBG">
         <div class="row">
             <div class="col-sm-12">
-                <h4>About animerooms</h4>
-                <p class="AboutText">Hello my name is Octavius the creator of animerooms.</p>
-                <p class="AboutText"></p>
+                <div class="AboutBG">
+                    <h4 class="AboutIntro">About animerooms</h4>
+                    <div class="AboutTextBG">
+                        <h5 class="AboutOpener">Hello my name is Octavius the creator of animerooms.</h5>
+                        <p class="AboutText">
+                            I work professionally as a web developer and since I was in 2012 have been a fan of anime, otaku and japanese culture for nearly the past decade and has been something I loved to spend my free time searching the interwebs checking out websites about.
+                            Unfortunately I haven't found a place where fans can show off our figures, manga, news, t-shirts, art and anime collections to the world.
+                            Some of my favorite shows are One Piece, Samurai Champloo, School Rumble, Jojo Bizarre adventure and Welcome to the NHK.
+                            <br>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;One day I would like to transform this website into a community hub where this can be possible, but currently I work a full time job and only have a limited amount of time in my day.
+                            This will be a journey that I’m looking forward too while this project is in development I would like to share my thoughts and highlight cool anime rooms, merchandise, art and news.
+                            So please give any feedback comments, support, suggestions or criticisms that you may have on the website and lets build a community together. :D
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
