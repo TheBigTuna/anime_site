3d1c32d0a5e8ccfd2fbdf8f76bfca29db98ac12d
octavius <mooreoctavius94@gmail.com>
2019-12-25 15:45:00 -0500

    update 12/25/2019 #2
    update 12/25/2019 #2

--- admin/insertPost.php
+++ admin/insertPost.php
@@ -2,25 +2,38 @@
     session_start();
     include('../resources/connection.php');
 
+    $Tag1 = "";
+    $Tag2 = "";
+    $Tag3 = "";
+    $Tag4 = "";
+    $Tag5 = "";
+    $Img1 = "";
+    $Img2 = "";
+    $Img3 = "";
+    $Img4 = "";
+    $Img5 = "";
+
+    
+
     // Post variables to be inserted into the cmsarticles table
-    $Title = $_POST['Title'];
-    $SubTitle = $_POST['SubTitle'];
-    $User = "Octavius";
-    $CurrentDate = date("Y-m-d H:i:s");
+    $Title = mysqli_real_escape_string($conn, $_POST['Title']);
+    $SubTitle = mysqli_real_escape_string($conn, $_POST['SubTitle']);
+    $User = mysqli_real_escape_string($conn, "Octavius");
+    $CurrentDate = mysqli_real_escape_string($conn, date("Y-m-d H:i:s"));
 
     // Post variables to be inserted into the cmsarticlesinfo table
-    $ArticleType = $_POST['PostType'];
-    $ArticleText = $_POST['PostText'];
-    $Tag1 = $_POST['Tags'][0];
-    $Tag2 = $_POST['Tags'][1];
-    $Tag3 = $_POST['Tags'][2];
-    $Tag4 = $_POST['Tags'][3];
-    $Tag5 = $_POST['Tags'][4];
-    $Img1 = $_POST['Img'][0];
-    $Img2 = $_POST['Img'][1];
-    $Img3 = $_POST['Img'][2];
-    $Img4 = $_POST['Img'][3];
-    $Img5 = $_POST['Img'][4];
+    $ArticleType = mysqli_real_escape_string($conn, $_POST['PostType']);
+    $ArticleText = mysqli_real_escape_string($conn, $_POST['PostText']);
+    $Tag1 = mysqli_real_escape_string($conn, $_POST['Tags'][0]);
+    $Tag2 = mysqli_real_escape_string($conn, $_POST['Tags'][1]);
+    $Tag3 = mysqli_real_escape_string($conn, $_POST['Tags'][2]);
+    $Tag4 = mysqli_real_escape_string($conn, $_POST['Tags'][3]);
+    $Tag5 = mysqli_real_escape_string($conn, $_POST['Tags'][4]);
+    $Img1 = mysqli_real_escape_string($conn, $_POST['Img'][0]);
+    $Img2 = mysqli_real_escape_string($conn, $_POST['Img'][1]);
+    $Img3 = mysqli_real_escape_string($conn, $_POST['Img'][2]);
+    $Img4 = mysqli_real_escape_string($conn, $_POST['Img'][3]);
+    $Img5 = mysqli_real_escape_string($conn, $_POST['Img'][4]);
 
     // Begin transaction
     // mysqli_begin_transaction($conn, MYSQLI_TRANS_START_READ_ONLY);
@@ -35,6 +48,8 @@
     }
     // Adds one to the largest ID available
     $ID++;
+
+    $ID = mysqli_real_escape_string($conn, $ID);
 
     // Insert into article results table
     $InsertArticle = "INSERT INTO omoore94_animerooms.cmsarticles (ID, ArticleName, ArticleSubTitle, User, Timestamp) VALUES ('$ID', '$Title', '$SubTitle', '$User', '$CurrentDate'); ";  
