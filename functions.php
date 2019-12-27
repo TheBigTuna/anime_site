3582d49c02db20ae5a6af1190a99830172ac9fca
octavius <mooreoctavius94@gmail.com>
2019-12-27 01:38:18 -0500

    update 12/27/2019 #2
    update 12/27/2019 #2

--- functions.php
+++ functions.php
@@ -10,6 +10,9 @@
         // $Article = substr_replace($Article, "<span style='font-size:16px; font-weight: 600;'>....</span><a href='/anime_site/article.php?ID=" . $Article . "'><span style='font-size:13px; color: #1bb1dc;'> Read More </span></a>", 250);
         // Creates break tags in between paragraphs
         $Article = str_replace( "\n", '<br />' . str_repeat('&nbsp;', 5), $Article);
+        $Article = str_replace( "<b>", "<b>", $Article);
+        $Article = str_replace( "</b>", "</b>", $Article);
+
         // Adds indentation for new paragraphs
         $Article = str_replace( "\n", str_repeat('&nbsp;', 5), $Article);
         
