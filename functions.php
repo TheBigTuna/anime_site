8ab6817c7cb457e497e4fa62f8137ac87243e2a4
octavius <mooreoctavius94@gmail.com>
2019-12-27 23:36:04 -0500

    update 12/27/2019 #4
    update 12/27/2019 #4

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
-// Function to verify admins 
+    // Function to verify admins 
     function verifyAdmin(){
         if(!isset($_SESSION['IsAdmin'])){
-            header('Location: /anime_site/index.php');
+            echo "<script>window.location.href = '/anime_site/index.php';</script>";
         }
     }
 
