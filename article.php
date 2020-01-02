1983ee7474803361eb930f6ca0000c9b74faf47b
octavius <mooreoctavius94@gmail.com>
2020-01-01 23:07:11 -0500

    update 1/1/2020 #2
    update 1/1/2020 #2

--- article.php
+++ article.php
@@ -3,84 +3,84 @@
 ?>
 <div class="row">
     <div class="container">
-                <div class="ArticlePageBG">
-                    <!-- Article Title -->
-                    <div class="ArticlePageTitleBG">
-                        <h2 class="ArticlePageTitle"><?= $ArticleRow[0]['ArticleName']; ?></h2>
-                    </div>
-                    <!-- Article Author -->
-                    <h6 class="ArticlePageAuthor">By <span style="color: #0B5AA3;"><?= $ArticleRow[0]['User']; ?></span></h6>
-                    <!-- Timestamp from when the article was created -->
-                    <h6 class="ArticlePageTimestamp"><?= date("F d, Y g:i A",strtotime($ArticleRow[0]['Timestamp'])); ?></h6>
-                    <div id="ArticleSection">
+        <div class="ArticlePageBG">
+            <!-- Article Title -->
+            <div class="ArticlePageTitleBG">
+                <h2 class="ArticlePageTitle"><?= $ArticleRow[0]['ArticleName']; ?></h2>
+            </div>
+            <!-- Article Author -->
+            <h6 class="ArticlePageAuthor">By <span style="color: #0B5AA3;"><?= $ArticleRow[0]['User']; ?></span></h6>
+            <!-- Timestamp from when the article was created -->
+            <h6 class="ArticlePageTimestamp"><?= date("F d, Y g:i A",strtotime($ArticleRow[0]['Timestamp'])); ?></h6>
+            <div id="ArticleSection">
+                <div class="row">
+                    <div class="col-sm-12 col-lg-8">
+                        <!-- First picture for the current article -->
+                        <div class="ArticleMainPictureBG">
+                            <img src="images/<?= $ArticleRow[0]['Img1']; ?>" class="ArticleImg" style="height: 450px; width: 100%; object-fit: cover; float: right;">
+                        </div>
+                        <!-- List tagged categories for the current article -->
+                        <div class="ArticleListTags">
+                            <ul>
+                                <a href="#"><li><?= $ArticleRow[0]['Tag1']; ?></li></a>
+                                <a href="#"><li><?= $ArticleRow[0]['Tag2']; ?></li></a>
+                                <a href="#"><li><?= $ArticleRow[0]['Tag3']; ?></li></a>
+                                <a href="#"><li><?= $ArticleRow[0]['Tag4']; ?></li></a>
+                                <a href="#"><li><?= $ArticleRow[0]['Tag5']; ?></li></a>
+                            </ul>
+                        </div>
+                        
                         <div class="row">
-                            <div class="col-sm-12 col-lg-8">
-                                <!-- First picture for the current article -->
-                                <div class="ArticleMainPictureBG">
-                                    <img src="images/<?= $ArticleRow[0]['Img1']; ?>" class="ArticleImg" style="height: 450px; width: 100%; object-fit: cover; float: right;">
-                                </div>
-                                <!-- List tagged categories for the current article -->
-                                <div class="ArticleListTags">
-                                    <ul>
-                                        <a href="#"><li><?= $ArticleRow[0]['Tag1']; ?></li></a>
-                                        <a href="#"><li><?= $ArticleRow[0]['Tag2']; ?></li></a>
-                                        <a href="#"><li><?= $ArticleRow[0]['Tag3']; ?></li></a>
-                                        <a href="#"><li><?= $ArticleRow[0]['Tag4']; ?></li></a>
-                                        <a href="#"><li><?= $ArticleRow[0]['Tag5']; ?></li></a>
-                                    </ul>
-                                </div>
-                                
-                                <div class="row">
-                                    <div class="col-sm-12 col-lg-12">
-                                        <div class="ArticleBodyContainer">
-                                            <div class="ArticleSubTitleBg">
-                                                <h6 class="ArticleSubTitle"><?= $ArticleRow[0]['ArticleSubTitle']; ?></h6>
-                                            </div>
-                                            <div class="ArticleTextBg">
-                                                <p id="ArticleText"><?php SplitArticle($ArticleRow[0]['Text'], $ArticleRow[0]['ArticleType'], $ImageArray); ?></p>
-                                            </div>
-                                        </div>
+                            <div class="col-sm-12 col-lg-12">
+                                <div class="ArticleBodyContainer">
+                                    <div class="ArticleSubTitleBg">
+                                        <h6 class="ArticleSubTitle"><?= $ArticleRow[0]['ArticleSubTitle']; ?></h6>
+                                    </div>
+                                    <div class="ArticleTextBg">
+                                        <p id="ArticleText"><?php SplitArticle($ArticleRow[0]['Text'], $ArticleRow[0]['ArticleType'], $ImageArray); ?></p>
                                     </div>
                                 </div>
-
                             </div>
-                            <div class="col-sm-12 col-lg-4">
-                                <div class="ArticleSuggestedArticlesBG">
-                                    <div class="row">
-                                        <div class="col-sm-12">
-                                            <h5 class="ArticleRecentArticlesTextTop">Recent Articles</h5>
-                                        <?php
-                                            // Loop throught the recent articles session array
-                                            foreach($_SESSION['RecentArticles'] as $row){
-                                        ?>
-                                            <a href="/anime_site/article.php?ID=<?= $row['ID']; ?>">
-                                                <div class="row" style="margin: 20px 0px;">
-                                                    <div class="col-sm-12 col-md-5">
-                                                        <!-- Recent Article Images -->
-                                                        <div class="ArticleRecentImagesContainer">
-                                                            <img src="images/<?= $row['Img1']; ?>" style="height: 90px; width: 100%; float: right; object-fit: cover;">
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-sm-12 col-md-7">
-                                                        <!-- Recent Article Titles -->
-                                                        <div class="ArticleRecentTitleContainer">
-                                                            <h6 class="ArticleRecentTitle"><?= $row['ArticleName']; ?></h6>
-                                                        </div>
-                                                    </div>
+                        </div>
+
+                    </div>
+                    <div class="col-sm-12 col-lg-4">
+                        <div class="ArticleSuggestedArticlesBG">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <h5 class="ArticleRecentArticlesTextTop">Recent Articles</h5>
+                                <?php
+                                    // Loop throught the recent articles session array
+                                    foreach($_SESSION['RecentArticles'] as $row){
+                                ?>
+                                    <a href="/anime_site/article.php?ID=<?= $row['ID']; ?>">
+                                        <div class="row" style="margin: 20px 0px;">
+                                            <div class="col-sm-12 col-md-5">
+                                                <!-- Recent Article Images -->
+                                                <div class="ArticleRecentImagesContainer">
+                                                    <img src="images/<?= $row['Img1']; ?>" style="height: 90px; width: 100%; float: right; object-fit: cover;">
                                                 </div>
-                                            </a>
-                                        <?php
-                                            }
-                                        ?>
+                                            </div>
+                                            <div class="col-sm-12 col-md-7">
+                                                <!-- Recent Article Titles -->
+                                                <div class="ArticleRecentTitleContainer">
+                                                    <h6 class="ArticleRecentTitle"><?= $row['ArticleName']; ?></h6>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
+                                    </a>
+                                <?php
+                                    }
+                                ?>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div> 
-        </div> 
+            </div>
+        </div>
+    </div> 
+</div> 
 <?php
     include('footer.php');
 ?>
--- resources/pageInfo.php
+++ resources/pageInfo.php
@@ -4,40 +4,40 @@
 // Depending on the page apply the appropiate title
 if(strpos($_SERVER['REQUEST_URI'],"/admin") > -1){
     $_SESSION['CurrentPage'] = "Admin - animerooms.com";
-    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime and anime merchandise.";
+    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime.";
     if(strpos($_SERVER['REQUEST_URI'],"index.php") == False){
         verifyAdmin();
     }
 }
 else if(strpos($_SERVER['REQUEST_URI'],"/index.php")){
     $_SESSION['CurrentPage'] = "Home - animerooms.com";
-    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime and anime merchandise.";
+    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime.";
 }
 else if(strpos($_SERVER['REQUEST_URI'],"/lifestyle.php")){
     $_SESSION['CurrentPage'] = "Lifestyle - animerooms.com";
-    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime and anime merchandise.";
+    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime.";
 }
 else if(strpos($_SERVER['REQUEST_URI'],"/contact.php")){
     $_SESSION['CurrentPage'] = "Contact - animerooms.com";
-    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime and anime merchandise.";
+    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime.";
 }
 else if(strpos($_SERVER['REQUEST_URI'],"/anime.php")){
     $_SESSION['CurrentPage'] = "Anime - animerooms.com";
-    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime and anime merchandise.";
+    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime.";
 }
 else if(strpos($_SERVER['REQUEST_URI'],"/manga.php")){
     $_SESSION['CurrentPage'] = "Manga - animerooms.com";
-    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime and anime merchandise.";
+    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime.";
 }
 else if(strpos($_SERVER['REQUEST_URI'],"/about.php")){
     $_SESSION['CurrentPage'] = "About - animerooms.com";
-    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime and anime merchandise.";
+    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime.";
 }
 else if(strpos($_SERVER['REQUEST_URI'],"/news.php")){
     $_SESSION['CurrentPage'] = "News - animerooms.com";
-    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime and anime merchandise.";
+    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime.";
 }
-else if(strpos($_SERVER['REQUEST_URI'],"/article.php")){
+else if(strpos($_SERVER['REQUEST_URI'],"/article.php") > -1){
     // Query to fetch currently available articles
     $ID = mysqli_real_escape_string($conn, $_GET['ID']);
     $FetchArticles = "SELECT * FROM omoore94_animerooms.cmsarticles AS A INNER JOIN omoore94_animerooms.cmsarticlesinfo AS AI ON AI.ID = A.ID WHERE A.ID = $ID ORDER BY A.ID DESC";
@@ -53,8 +53,8 @@ else if(strpos($_SERVER['REQUEST_URI'],"/article.php")){
     $ImageArray = array($ArticleRow[0]['Img1'], $ArticleRow[0]['Img2'], $ArticleRow[0]['Img3'],$ArticleRow[0]['Img4'],$ArticleRow[0]['Img5']);
 }
 else{
-    $_SESSION['CurrentPage'] = "Home - animerooms.com";
-    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime and anime merchandise.";
+    $_SESSION['CurrentPage'] = "animerooms.com";
+    $_SESSION['Description'] = "Hello animerooms is a blog dedicated to the love of anime.";
 }
 
 // If recent articles have not been stored create a recent articles variable and store the five most recent articles
