478a34017e5e5965dcc72431255e63b6f6d9679e
octavius <mooreoctavius94@gmail.com>
2020-01-06 17:30:43 -0500

    update 1/6/2020
    update 1/6/2020

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
     function splitArticle($Article, $Type, $Images){
         // $Article = substr_replace($Article, "<span style='font-size:16px; font-weight: 600;'>....</span><a href='/anime_site/article.php?ID=" . $Article . "'><span style='font-size:13px; color: #1bb1dc;'> Read More </span></a>", 250);
         // Creates break tags in between paragraphs
-        $Article = str_replace( "\n", '<br />' . str_repeat('&nbsp;', 5), $Article);
+        $Article = str_replace( "\n", '<br />', $Article);
         $Article = str_replace( "<b>", "<b>", $Article);
         $Article = str_replace( "</b>", "</b>", $Article);
         $Article = str_replace( "<i>", "<i>", $Article);
@@ -34,13 +34,13 @@
                 break;
             case "2":
                 if(strpos($Article, "!Image2!") == true){
-                    $Article = str_replace( "!Image2!", "<img src='images/" . $Images[1] . "'style='height: 380px; width: 90%; display: block; margin: 0 auto; object-fit: cover;'>", $Article);
+                    $Article = str_replace( "!Image2!", "<img src='images/" . $Images[1] . "'style='height: 440px; width: 90%; display: block; margin: 0 auto; object-fit: cover;'>", $Article);
                 }
                 if(strpos($Article, "!Image3!") == true){
-                    $Article = str_replace( "!Image3!", "<img src='images/" . $Images[2] . "'style='height: 380px; width: 90%; display: block; margin: 0 auto; object-fit: cover;'>", $Article);
+                    $Article = str_replace( "!Image3!", "<img src='images/" . $Images[2] . "'style='height: 440px; width: 90%; display: block; margin: 0 auto; object-fit: cover;'>", $Article);
                 }
                 if(strpos($Article, "!Image4!") == true){
-                    $Article = str_replace( "!Image4!", "<img src='images/" . $Images[3] . "'style='height: 380px; width: 90%; display: block; margin: 0 auto; object-fit: cover;'>", $Article);
+                    $Article = str_replace( "!Image4!", "<img src='images/" . $Images[3] . "'style='height: 440px; width: 90%; display: block; margin: 0 auto; object-fit: cover;'>", $Article);
                 }
                 break;
             case "3":
